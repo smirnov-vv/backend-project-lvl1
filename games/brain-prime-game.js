2ecb21f7ef0681771ce5be4bae10e6168128dbe8
smirnov-vv <vsmirnov.pro@gmail.com>
2022-12-31 16:22:08 +0300

    fix: algorithm mistake

--- games/brain-prime-game.js
+++ games/brain-prime-game.js
@@ -4,6 +4,9 @@ export default () => {
   const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const rightResult = (num) => {
+    if (num === 1) {
+      return 'no';
+    }
     for (let i = 2; i < num; i += 1) {
       if (num % i === 0) {
         return 'no';
